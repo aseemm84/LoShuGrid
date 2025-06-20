51c4bbee2dc49411e8eac1d83332384349122376
Aseem Mehrotra <67800004+aseemm84@users.noreply.github.com>
2025-06-20 20:17:24 +0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
-from LoShu_backend import calculate_numbers, build_grid_dataframe, check_planes, generate_interpretation
+import LoShu_backend
 
 # --- Streamlit Page Configuration ---
 st.set_page_config(
